00e95f1b4a2c86c87ff6bb7e4114b78400630574
LujanSolo <lujansolo@proton.me>
2023-07-18 18:21:13 -0700

    reduced repetitive code with renderCharacter function

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,19 +14,18 @@ const monster = {
   diceRoll: 4
 }
 
-document.getElementById('hero').innerHTML = `
-  <div class="character-card">
-    <h4 class="name">Wizard</h4>
-    <img class="avatar" src="assets/images/wizard.png" alt="fantasy wizard character" loading="lazy"/>
-    <p class="health">health: <b> 60 </b></p>
-    <div class="dice-container"><div class="dice">6</div></div>
-  </div>
-`;
+function renderCharacter(data) {
+  document.getElementById(data.elementId).innerHTML = `
+    <div class="character-card">
+      <h4 class="name">${data.name}</h4>
+      <img class="avatar" src=${data.avatar} alt="fantasy ${name} caricature" />
+      <p class="health">Health: <b>${data.health}</b></p>
+      <div class="dice-container">
+        <div class="dice">${data.diceRoll}</div>
+      </div>
+    </div>
+  `
+}
 
-document.getElementById('monster').innerHTML = `
-  <div class="character-card">
-  <h4 class="name"> Orc </h4>
-  <img class="avatar" src="assets/images/orc.png"   alt="fantasy orc character" loading="lazy"/>
-  <p class="health">health: <b> 10 </b></p>
-  <div class="dice-container"><div class="dice"> 4 </div></div>
-`;
+renderCharacter(hero);
+renderCharacter(monster);
