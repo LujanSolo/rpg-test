8358d2daf67744041d0f30df4abe64b31d417a8c
LujanSolo <lujansolo@proton.me>
2023-07-19 12:09:04 -0700

    added temporary arrays to diceRoll and added those as iterated items to the for loop

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,8 @@ const hero = {
   name: "Wizard",
   avatar: "assets/images/wizard.png",
   health: 60,
-  diceRoll: 6
+  diceRoll: [6, 3, 1],
+  diceCount: 3
 }
 
 const monster = {
@@ -11,18 +12,23 @@ const monster = {
   name: "Orc",
   avatar: "assets/images/orc.png",
   health: 10,
-  diceRoll: 4
+  diceRoll: [4],
+  diceCount: 1
 }
 
 function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceRoll } = data;
+  const { elementId, name, avatar, health, diceRoll, diceCount } = data;
+  let diceHtml = '';
+  for (i = 0; i < diceCount; i++) {
+    diceHtml += `<div class="dice">${diceRoll[i]}</div>`;
+  }
   document.getElementById(elementId).innerHTML = `
     <div class="character-card">
       <h4 class="name">${name}</h4>
       <img class="avatar" src=${avatar} alt="fantasy ${name} caricature" />
       <p class="health">Health: <b>${health}</b></p>
       <div class="dice-container">
-        <div class="dice">${diceRoll}</div>
+        ${diceHtml}
       </div>
     </div>
   `
