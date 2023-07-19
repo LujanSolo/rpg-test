dbdb5eaa19bfc5e1619360e01e499822a2ec9cfe
LujanSolo <lujansolo@proton.me>
2023-07-18 17:59:52 -0700

    added js to add characters to HTML dynamically

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+const heroCard = document.getElementById('hero');
+const monsterCard = document.getElementById('monster');
+
+heroHtml = `
+  <div class="character-card">
+    <h4 class="name">Wizard</h4>
+    <img class="avatar" src="assets/images/wizard.png" alt="fantasy wizard character" loading="lazy"/>
+    <p class="health">health: <b> 60 </b></p>
+    <div class="dice-container"><div class="dice">6</div></div>
+  </div>
+`;
+
+monsterHtml =  `
+  <div class="character-card">
+  <h4 class="name"> Orc </h4>
+  <img class="avatar" src="assets/images/orc.png"   alt="fantasy orc character" loading="lazy"/>
+  <p class="health">health: <b> 10 </b></p>
+  <div class="dice-container"><div class="dice"> 4 </div></div>
+`
+heroCard.innerHTML = heroHtml;
+monsterCard.innerHTML = monsterHtml;
