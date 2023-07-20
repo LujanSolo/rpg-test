2b2406d88af50fc1d7447b350f72f04a17036a22
LujanSolo <lujansolo@proton.me>
2023-07-20 12:46:19 -0700

    added new data.js file to hold character data. moved data and set import/export relationship

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,13 @@
+import characterData from './data.js';
+
 function getDiceRollArray(diceCount) {
   return new Array(diceCount).fill(0).map(() => {
-    return Math.floor(Math.random() * 6) + 1
-  })
-};
-
-const characterData = {
-  hero: {
-    elementId: "hero",
-    name: "Wizard",
-    avatar: "assets/images/wizard.png",
-    health: 60,
-    diceCount: 3
-  },
-
-  monster: {
-    elementId: "monster",
-    name: "Orc",
-    avatar: "assets/images/orc.png",
-    health: 10,
-    diceCount: 1
-  },
-};
+    return Math.floor(Math.random() * 6) + 1;
+  });
+}
 
 function Character(data) {
-  Object.assign(this, data)
+  Object.assign(this, data);
   this.getCharacterHtml = () => {
     const { elementId, name, avatar, health, diceCount } = this;
     const diceHtml = this.getDiceHtml(diceCount);
@@ -37,19 +21,21 @@ function Character(data) {
         ${diceHtml}
       </div>
     </div>
-  `
+  `;
   };
 
   this.getDiceHtml = (diceCount) => {
-    return getDiceRollArray(diceCount).map((num) => {
-      return `<div class="dice">${num}</div>`
-    }).join("")
-  }
-};
+    return getDiceRollArray(diceCount)
+      .map((num) => {
+        return `<div class="dice">${num}</div>`;
+      })
+      .join("");
+  };
+}
 
-const wizard = new Character(characterData.hero)
-const orc = new Character(characterData.monster)
+const wizard = new Character(characterData.hero);
+const orc = new Character(characterData.monster);
 
 // separation of concerns
-document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml()
-document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml()
+document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml();
+document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
