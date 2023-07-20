932f81e60bb3242f2f26e9f8df13a544192aefbc
LujanSolo <lujansolo@proton.me>
2023-07-20 16:51:59 -0700

    added Character.js file and moved main function there. set up related import/export

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,7 @@
 import characterData from './data.js';
-import { getDiceRollArray } from './utils.js';
-
-
-function Character(data) {
-  Object.assign(this, data);
-  this.getCharacterHtml = () => {
-    const { elementId, name, avatar, health, diceCount } = this;
-    const diceHtml = this.getDiceHtml(diceCount);
-
-    return `
-    <div class="character-card">
-      <h4 class="name">${name}</h4>
-      <img class="avatar" src=${avatar} alt="fantasy ${name} caricature" />
-      <p class="health">Health: <b>${health}</b></p>
-      <div class="dice-container">
-        ${diceHtml}
-      </div>
-    </div>
-  `;
-  };
-
-  this.getDiceHtml = (diceCount) => {
-    return getDiceRollArray(diceCount)
-      .map((num) => {
-        return `<div class="dice">${num}</div>`;
-      })
-      .join("");
-  };
-}
-
+import Character from './Character.js';
 const wizard = new Character(characterData.hero);
 const orc = new Character(characterData.monster);
 
-// separation of concerns
 document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml();
 document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,4 +1,4 @@
-function getDiceRollArray(diceCount) {
+ function getDiceRollArray(diceCount) {
   return new Array(diceCount).fill(0).map(() => {
     return Math.floor(Math.random() * 6) + 1;
   });
