6126a4aaa9bdeed72ced10c2a726b3f461e40a18
LujanSolo <lujansolo@proton.me>
2023-07-29 16:50:30 -0700

    changed constructor function to a class

--- assets/js/Character.js
+++ assets/js/Character.js
@@ -1,19 +1,20 @@
 import { diceRollArray, dicePlaceholderHtml, getPercentage } from './utils.js';
 
-function Character(data) {
-  Object.assign(this, data)
-
-  this.diceHtml = dicePlaceholderHtml(this.diceCount);
-  this.maxHealth = this.health;
-
-  this.setDiceHtml = () => {
+class Character {
+  constructor(data){
+    Object.assign(this, data)
+    this.diceHtml = dicePlaceholderHtml(this.diceCount);
+    this.maxHealth = this.health;
+  };
+  
+  setDiceHtml() {
     this.currentDiceScore = diceRollArray(this.diceCount);
     this.diceHtml = this.currentDiceScore.map((num) =>
       `<div class="dice">${num}</div>`
     ).join('');
   };
 
-  this.takeDamage = (attackScoreArray) => {
+  takeDamage(attackScoreArray) {
     const totalAttackScore = attackScoreArray.reduce((total, currentElement) => total + currentElement);
     this.health -= totalAttackScore;
     if (this.health < 0) {
@@ -22,7 +23,7 @@ function Character(data) {
     }
   };
 
-  this.getHealthBar = () => {
+  getHealthBar() {
     const percent = getPercentage(this.health, this.maxHealth);
     return `
       <div class="health-bar-outer">
@@ -33,7 +34,7 @@ function Character(data) {
     `
   }
 
-  this.getCharacterHtml = () => {
+  getCharacterHtml() {
     const { name, avatar, health, diceHtml } = this;
     const healthBar = this.getHealthBar();
     return `
@@ -48,5 +49,54 @@ function Character(data) {
         </div>`;
   };
 };
+
+// function Character(data) {
+//   Object.assign(this, data)
+
+//   this.diceHtml = dicePlaceholderHtml(this.diceCount);
+//   this.maxHealth = this.health;
+
+//   this.setDiceHtml = () => {
+//     this.currentDiceScore = diceRollArray(this.diceCount);
+//     this.diceHtml = this.currentDiceScore.map((num) =>
+//       `<div class="dice">${num}</div>`
+//     ).join('');
+//   };
+
+//   this.takeDamage = (attackScoreArray) => {
+//     const totalAttackScore = attackScoreArray.reduce((total, currentElement) => total + currentElement);
+//     this.health -= totalAttackScore;
+//     if (this.health < 0) {
+//       this.dead = true;
+//       this.health = 0;
+//     }
+//   };
+
+//   this.getHealthBar = () => {
+//     const percent = getPercentage(this.health, this.maxHealth);
+//     return `
+//       <div class="health-bar-outer">
+//         <div class="health-bar-inner ${percent <= 25 ? "danger" : ""} " 
+//         style="width: ${percent}%;">
+//         </div>
+//       </div>
+//     `
+//   }
+
+//   this.getCharacterHtml = () => {
+//     const { name, avatar, health, diceHtml } = this;
+//     const healthBar = this.getHealthBar();
+//     return `
+//       <div class="character-card">
+//         <h4 class="name"> ${name} </h4>
+//         <img class="avatar" src="${avatar}" />
+//         <div class="health">health: <b> ${health} </b></div>
+//         ${healthBar}
+//         <div class="dice-container">
+//             ${diceHtml}
+//         </div>
+//         </div>`;
+//   };
+// };
 
 export default Character;
