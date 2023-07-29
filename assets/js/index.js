5b507dafb2d71f0f34765faf1b7b04d654246ec7
LujanSolo <lujansolo@proton.me>
2023-07-28 22:37:39 -0700

    added getNextOpponent function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,16 @@
 import characterData from './data.js';
 import Character from './Character.js';
 
+const opponentArray = ["orc", "demon", "goblin"];
 
 const wizard = new Character(characterData.hero);
 const orc = new Character(characterData.monster);
 
 document.getElementById('attack-btn').addEventListener('click', attack);
 
-// function getOpponent() {
-//   const nextOpponentData = 
-// }
+function getNextOpponent() {
+  const nextCharData = opponentArray.shift().characterHtml();
+}
 
 function attack() {
   orc.getDiceHtml();
